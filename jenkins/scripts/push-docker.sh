953708dcfc652904c84863d2693595781ff523c7
Euan Kemp <euank@euank.com>
2017-04-29 14:13:22 -0700

    Update repo again

--- jenkins/scripts/push-docker.sh
+++ jenkins/scripts/push-docker.sh
@@ -3,6 +3,6 @@ set -ex
 set -u
 set -o pipefail
 
-REPO=euank/alexa-parrot:$(git rev-parse --short HEAD)
+REPO=quay.io/euank/alexa-parrot:$(git rev-parse --short HEAD)
 docker build -t "${REPO}" .
 docker push "${REPO}"
